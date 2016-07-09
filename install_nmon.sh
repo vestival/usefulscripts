78f81fc88620cb57cf60cab3cd0d09004841eefc
vestival <victor.estival@gmail.com>
2016-07-09 20:45:43 +0200

    Create install_nmon.sh

---
+++ install_nmon.sh
@@ -0,0 +1,6 @@
+cd /tmp
+wget http://sourceforge.net/projects/nmon/files/nmon16e_mr_nmon.tar.gz
+tar -zxvf  nmon16e_mr_nmon.tar.gz
+cp nmon16e_x86_rhel72 /usr/local/bin/
+chmod a+x /usr/local/bin/nmon16e_x86_rhel72
+ln -s /usr/local/bin/nmon16e_x86_rhel72 /usr/local/bin/nmon
